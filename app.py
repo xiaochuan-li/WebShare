fb44d8238532e5dbf1999160e569fa2cf5e5e715
xiaochuan-li <lixiaochuan822@gmail.com>
2020-12-27 00:57:09 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template, request, make_response, send_from_directory, redirect
+import random
+import string
+import os
+import time
+
+import multiprocessing as mp
+
+def uuid():
+  return ''.join(random.sample(string.ascii_letters + string.digits, 20))
+app = Flask(__name__)
+
+ROOT = os.getcwd()
+SHAREROOT = os.path.join(ROOT, "shared")
+
+def add_removal(did, internal = 15, dir_ = "remove.txt"):
+  with open(os.path.join(ROOT, dir_),"a", encoding="utf-8") as f:
+    f.write("{} {}\n".format(did, time.time()+internal))
+
+class Share(object):
+  def __init__(self, did):
+    self.did = did
+    self.didRoot = os.path.join(SHAREROOT, did)
+    os.mkdir(self.didRoot)
+    self.txtFile = os.path.join(self.didRoot, "temp.txt")
+    add_removal(did)
+  def writeFiles(self, files):
+    with open(self.txtFile,"w", encoding="utf-8") as f:
+      for file_ in files:
+        file_.save("{}/{}".format(self.didRoot, file_.filename))
+        f.write("{} {}\n".format(file_.filename, "/download/{}/{}".format(self.did, file_.filename)))    
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title="Home", home_status="active")
+
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html', title="Upload", upload=True, upload_status = "active")
+
+@app.route('/download')
+def download():
+   return render_template('download.html', title="Download", download_status = "active")
+
+@app.route('/list_from_code',methods=['GET',"POST"])
+def list_from_code():
+  if request.method == 'POST':
+    did = request.form.get('code')
+    return redirect("/listfiles/"+did)
+  return render_template('download.html', title="Download",download_status = "active")
+
+
+@app.route('/listfiles/<did>')
+def listfiles(did):
+  if not os.path.isfile(SHAREROOT+"/{}/temp.txt".format(did)):
+    return render_template('download.html', title="Download", download_status = "active", alertMSG = "Code NOT Valid")
+  files=[]
+  with open(SHAREROOT+"/{}/temp.txt".format(did),"r", encoding="utf-8") as f:
+    for line in f.readlines():
+      name, url = line.split(" ")
+      files.append((name.strip(),url.strip()))
+  return render_template('filelist.html', title="Download", download_status="active", data = {"files":files})
+
+@app.route("/download/<did>/<filename>", methods=['GET'])
+def download_file(did,filename):
+    response = make_response(send_from_directory(os.path.join(SHAREROOT,did), filename, as_attachment=True))
+    response.headers["Content-Disposition"] = "attachment; filename={}".format(filename.encode().decode('latin-1'))
+    return response
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def uploader():
+  if request.method == 'POST':
+    did = uuid()
+    share = Share(did)
+    share.writeFiles(request.files.getlist('file'))
+    return render_template('upload.html', title = "Share", share=True,upload_status = "active", data = {"share_url":"/listfiles/"+did})
+  return render_template('upload.html', title="Upload", upload=True, upload_status = "active")
+
+if __name__ == '__main__':
+  app.run(host='127.0.0.1', port=8080, debug=True, threaded=True)
+
+ 
---
+++ remove.py
@@ -0,0 +1,26 @@
+import os
+import time
+import schedule
+import shutil
+ROOT = os.getcwd()
+SHAREROOT = os.path.join(ROOT, "shared")
+
+def remove(dir_ = "remove.txt"):
+    data =[]
+    if os.path.isfile(os.path.join(ROOT, dir_)):
+        with open(os.path.join(ROOT, dir_),"r", encoding="utf-8") as f:
+            for line in f.readlines():
+                did, t = line.split()
+                did, t = did.strip(), float(t.strip())
+                if t < time.time():
+                    shutil.rmtree(os.path.join(SHAREROOT, did), True)
+                else :
+                    data.append(line)
+                    
+        with open(os.path.join(ROOT, dir_),"w", encoding="utf-8") as f:
+            f.write("".join(data))
+
+if __name__ == "__main__":
+    schedule.every(10).seconds.do(remove)
+    while True:
+        schedule.run_pending()
